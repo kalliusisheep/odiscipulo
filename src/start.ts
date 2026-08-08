c1f069344bd94c23b207842a845dda58635e0ffb
kalliusisheep <kallius.impetus@gmail.com>
2026-08-08 14:10:52 -0300

    fix: import middleware from tanstack core

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,5 @@
-import { createStart, createMiddleware } from "@tanstack/react-start";
+import { createStart } from "@tanstack/react-start";
+import { createMiddleware } from "@tanstack/start-client-core";
 
 import { renderErrorPage } from "./lib/error-page";
 import { attachSupabaseAuth } from "@/integrations/supabase/auth-attacher";
